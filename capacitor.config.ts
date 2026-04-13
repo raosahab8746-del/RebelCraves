099b605f18da993e683aa111a0d6cda04ade228b
Hemant Yadav <raosahab8746@gmail.com>
2026-04-13 19:45:30 +0530

    Your update message

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.rebelcraves.app',
+  appName: 'RebelCraves',
+  webDir: 'dist'
+};
+
+export default config;
